ffbd9fada5d6321e3771cb0250ad8826fad21762
MyCode07 <erem19992015@mail.ru>
2024-01-27 13:44:34 +0300

    add helper navigation

--- src/js/app.js
+++ src/js/app.js
@@ -16,3 +16,10 @@ stickyHeader();
 
 // Fancybox.bind("[data-fancybox]", {
 // });
+
+document.addEventListener('click', function (e) {
+    let targetEl = e.target;
+    if (targetEl.classList.contains('pages-close')) {
+        document.querySelector('.pages').classList.toggle('_hide');
+    }
+})
